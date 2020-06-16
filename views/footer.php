94865f33e05c44fd395703ee3928414c99d123e8
dlinhhoangnhung <60937131+dlinhhoangnhung@users.noreply.github.com>
2020-06-16 16:12:55 +0700

    thiết kế UI: footer, header, index, 404

---
+++ views/footer.php
@@ -0,0 +1,11 @@
+
+
+
+<footer class="text-center card-header" data-selected="true" data-label-id="0" style="transition: none 0s ease 0s; cursor: move; position: relative; top: 52px; ">
+	© 2019 - Anh Nguyen - FoIT (Hutech)
+</footer>
+</div>
+<script src="/lab3/assets/js/jquery-3.3.1.min.js"></script>
+<script src="/lab3/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ views/not-found.php
@@ -0,0 +1,8 @@
+<?php
+include_once("header.php");
+?>
+<h1 class=text-center>404 - NOT FOUND!</h1>
+<h5 class="text-danger text-center">Không tìm thấy trang!</h5>
+<?php
+include_once("footer.php");
+?>
